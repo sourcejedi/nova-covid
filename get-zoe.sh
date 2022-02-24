83d9bef7b713ac71ab6f4369b0610c7e483118c2
Alan Jenkins <alan.christopher.jenkins@gmail.com>
2022-02-24 21:37:20 +0000

    get-zoe.sh: simple gsutil script to grab incidence_*.csv

---
+++ get-zoe.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# debug
+set -o xtrace
+
+# gsutil from google-cloud-sdk
+
+get() (
+    get="$1" &&
+    dir="download-zoe/$2" &&
+
+    mkdir -p "$dir" &&
+    cd "$dir" &&
+    gsutil -m cp -n "$get" .
+)
+
+get 'gs://covid-public-data/csv/incidence_202*.csv' 'incidence/'
