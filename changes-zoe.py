d493e740366a285b10766c6651ec5e11de41fdf3
Alan Jenkins <alan.christopher.jenkins@gmail.com>
2022-02-27 19:27:05 +0000

    changes-zoe.py: add start date

--- changes-zoe.py
+++ changes-zoe.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import os
 from pathlib import Path
+import csv
 
 outdir = Path('out/changes/')
 outdir.mkdir(parents=True, exist_ok=True)
@@ -10,13 +11,25 @@ def changes(indir, prefix, outfile):
     paths.sort()
     prefix_len = len(prefix)
     prev_head = None
+    prev_start = None
     for path in paths:
         with path.open() as f:
             head = f.readline()
-        head = head.rstrip()
+            head = head.rstrip()
+            f.seek(0)
+            read = csv.DictReader(f)
+            row = next(read)
+            assert row
+            assert 'date' in row
+            start = row['date']
+        name = path.name[prefix_len:-4]
         if head != prev_head:
-            outfile.write(f'{path.name[prefix_len:-4]}  {head}\n')
+            outfile.write(f'{name}\tField headings: {head}\n')
             prev_head = head
+        if start != prev_start:
+            outfile.write(f'{name}\t    Start date: {start}\n')
+            prev_start = start
+    outfile.write(f'{name}\n')
 
 indir = Path('download-zoe/incidence/')
 with open(outdir / 'changes_incidence', 'w') as outfile:
