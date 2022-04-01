b2a4b4f5dfacfec41817e7d67b40f307561f4a30
Alan Jenkins <alan.christopher.jenkins@gmail.com>
2022-04-01 16:52:15 +0100

    prevalence_from_incidence

---
+++ prevalence_from_incidence/incidence.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+
+import csv
+import math
+import scipy.special
+import sys
+
+def iter_rows(infile):
+    inc_reader = csv.DictReader(infile)
+    for row in inc_reader:
+        yield row
+
+# { ISO date, absolute incidence i.e. number of new occurences }
+def iter_incidence(infile, region):
+    for row in iter_rows(infile):
+        if region is None and 'region' in row.keys():
+            sys.exit("Please pass a region to select")
+        if 'region' in row.keys() and row['region'] != region:
+            continue
+            
+        pop_mid = float(row.get('pop_mid') or row.get('covid_in_pop'))
+        #assert pop_mid == int(pop_mid)
+        #pop_mid = int(pop_mid)
+
+        yield {'date': row['date'], 'incidence_mid': pop_mid}
+
+# Recovery model (days)
+RECOVERY_LEN=29
+
+def gamma_dist(x, a, B):
+    # ??? problem with the definition
+    # either the one I used, or the one implied by the CSS paper :-P
+    B=1/B
+
+    return B**a * x**(a-1) * math.e**(-B*x) / scipy.special.gamma(a)
+RECOVERY=[]
+
+y_cum = 0
+# x = days to recovery
+for x in range(1, RECOVERY_LEN+1):
+    y = gamma_dist(x, a=2.595, B=4.48)
+    y_cum += y
+    #print (y,y_cum)
+    RECOVERY.append(1-y_cum)
+
+def prevalence(infile, region):
+    incidences = list(iter_incidence(infile, region))
+    dates = list(map(lambda r: r['date'], incidences))
+    incidences = list(map(lambda r: r['incidence_mid'], incidences))
+    
+    window = incidences[:RECOVERY_LEN]
+    for i in range(RECOVERY_LEN, len(incidences)):
+        incidence = incidences[i]
+        
+        prevalence = 0
+        for j in range(0, RECOVERY_LEN):
+            prevalence += window[j] * RECOVERY[RECOVERY_LEN-1-j]
+        prevalence += incidence
+        yield (dates[i], incidence, prevalence)
+
+        del window[0]
+        window.append(incidence)
+
+region = None
+if len(sys.argv) == 2:
+    region = sys.argv[1]
+if len(sys.argv) > 2:
+    sys.exit("Usage: ./incidence.py [REGION] < input.csv > output.csv")
+
+w = csv.writer(sys.stdout)
+w.writerows(prevalence(sys.stdin, region))
