f4fb1fc93b1400e5a2da226c5f29028bc2bf226c
Alan Jenkins <alan.christopher.jenkins@gmail.com>
2022-04-01 21:01:47 +0100

    prevalence_from_incidence: refactor to avoid treating day 0 specially

--- prevalence_from_incidence/incidence.py
+++ prevalence_from_incidence/incidence.py
@@ -24,20 +24,20 @@ def iter_incidence(infile, region):
 
         yield {'date': row['date'], 'incidence_mid': pop_mid}
 
-# Recovery model (days)
-RECOVERY_LEN=29
-
 def gamma_dist(x, a, B):
     # ??? problem with the definition
     # either the one I used, or the one implied by the CSS paper :-P
     B=1/B
 
     return B**a * x**(a-1) * math.e**(-B*x) / scipy.special.gamma(a)
+
+# Recovery model (days)
+RECOVERY_LEN=30
 RECOVERY=[]
 
 y_cum = 0
 # x = days to recovery
-for x in range(1, RECOVERY_LEN+1):
+for x in range(0, RECOVERY_LEN):
     y = gamma_dist(x, a=2.595, B=4.48)
     y_cum += y
     #print (y,y_cum)
@@ -48,18 +48,17 @@ def prevalence(infile, region):
     dates = list(map(lambda r: r['date'], incidences))
     incidences = list(map(lambda r: r['incidence_mid'], incidences))
     
-    window = incidences[:RECOVERY_LEN]
-    for i in range(RECOVERY_LEN, len(incidences)):
+    window = incidences[:RECOVERY_LEN-1]
+    for i in range(RECOVERY_LEN-1, len(incidences)):
         incidence = incidences[i]
+        window.append(incidence)
         
         prevalence = 0
         for j in range(0, RECOVERY_LEN):
-            prevalence += window[j] * RECOVERY[RECOVERY_LEN-1-j]
-        prevalence += incidence
+            prevalence += window[RECOVERY_LEN-1-j] * RECOVERY[j]
         yield (dates[i], incidence, prevalence)
-
         del window[0]
-        window.append(incidence)
+        
 
 region = None
 if len(sys.argv) == 2:
