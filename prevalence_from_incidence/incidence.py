9505924e727f5b06f8ce4ebf58f7246d33c78f9c
Alan Jenkins <alan.christopher.jenkins@gmail.com>
2022-04-06 16:48:29 +0100

    prevalence_from_incidence/: convert all regions, instead of selecting one
    I, uh, thought I should add up the England regions, to overlay the figure,
    since the paper says it treats each English region separately.
    
    Arguably this was overkill. The way the math works, I guess I could just
    have input the England sum.  I think I have a script for that already, or I
    could have done it easily.
    
    Also the paper graphs the "England" incidence i.e. without region
    weighting for some reason...
    
    But I wrote it, it doesn't lose anything, and I sent this version of the
    script in an email.  So let's commit it.

--- prevalence_from_incidence/incidence.py
+++ prevalence_from_incidence/incidence.py
@@ -5,33 +5,13 @@ import math
 from scipy.stats import gamma
 import sys
 
-def iter_rows(infile):
-    inc_reader = csv.DictReader(infile)
-    for row in inc_reader:
-        yield row
-
-# { ISO date, absolute incidence i.e. number of new occurences }
-def iter_incidence(infile, region):
-    for row in iter_rows(infile):
-        if region is None and 'region' in row.keys():
-            sys.exit("Please pass a region to select")
-        if 'region' in row.keys() and row['region'] != region:
-            continue
-            
-        pop_mid = float(row.get('pop_mid') or row.get('covid_in_pop'))
-        #assert pop_mid == int(pop_mid)
-        #pop_mid = int(pop_mid)
-
-        yield {'date': row['date'], 'incidence_mid': pop_mid}
-
-
 # Recovery model (days)
 RECOVERY_LEN=30
 RECOVERY=[]
 
 # Values shown in original study.
 # These don't exactly match current data files
-#gamma = gamma(a=2.595, scale=4.48)
+gamma = gamma(a=2.595, scale=4.48)
 
 # Match current data files.
 # stdev of the error is about 0.5%
@@ -41,7 +21,7 @@ RECOVERY=[]
 # which provides fractional incidence.
 #
 #gamma = gamma(a=4.7, scale=2.88)
-gamma = gamma(a=4.5, scale=3.012)
+#gamma = gamma(a=4.5, scale=3.012)
 
 # Fit to UK. Except you can't do that, oops.
 # You need to do each region seperately.
@@ -49,18 +29,13 @@ gamma = gamma(a=4.5, scale=3.012)
 
 
 # x = days to recover
-y_cum = 0
 for x in range(0, RECOVERY_LEN):
-    y = gamma.pdf(x)
     y_cum = gamma.cdf(x)
-    #print (y,y_cum)
     RECOVERY.append(1-y_cum)
 
-def prevalence(infile, region):
-    incidences = list(iter_incidence(infile, region))
-    dates = list(map(lambda r: r['date'], incidences))
-    incidences = list(map(lambda r: r['incidence_mid'], incidences))
-    
+
+def prevalence(incidences):
+    prevalences = []
     window = incidences[:RECOVERY_LEN-1]
     for i in range(RECOVERY_LEN-1, len(incidences)):
         incidence = incidences[i]
@@ -69,15 +44,68 @@ def prevalence(infile, region):
         prevalence = 0
         for j in range(0, RECOVERY_LEN):
             prevalence += window[RECOVERY_LEN-1-j] * RECOVERY[j]
-        yield (dates[i], incidence, prevalence)
+        prevalences.append(prevalence)
         del window[0]
-        
+    return prevalences
 
-region = None
-if len(sys.argv) == 2:
-    region = sys.argv[1]
-if len(sys.argv) > 2:
-    sys.exit("Usage: ./incidence.py [REGION] < input.csv > output.csv")
+def sniff_regions(infile):
+    regions = []
+    read = csv.DictReader(infile)
+    row = next(read, None)
+    assert row
+    date = row.get('date')
+    assert(date)
+    start = date
+    while date == start:
+        region = row.get('region')
+        assert region
+        assert region not in regions
+        regions.append(region)
+        row = next(read, None)
+        assert row
+        date = row.get('date')
+    regions.sort()
+    infile.seek(0)
+    return regions
 
-w = csv.writer(sys.stdout)
-w.writerows(prevalence(sys.stdin, region))
+def write_prevalence(infile, outfile):
+    regions = sniff_regions(infile)
+    for country in 'England', 'Wales', 'Scotland', 'Northern Ireland', 'UK':
+        if country in regions:
+            regions.remove(country)
+            regions.append(country)
+
+    dates = []
+    incidences = {}
+    for region in regions:
+        incidences[region] = []
+
+    reader = csv.DictReader(infile)
+    prev_date = None
+    for row in reader:
+        date = row['date']
+        if date != prev_date:
+            prev_date = date
+            dates.append(date)
+        region = row['region']
+        pop_mid = float(row.get('pop_mid') or row.get('covid_in_pop'))
+        incidences[region].append(pop_mid)
+
+    prevalences = {}
+    for region in regions:
+        prevalences[region] = prevalence(incidences[region])
+    dates = dates[RECOVERY_LEN-1:]
+
+    w = csv.writer(sys.stdout)
+    w.writerow(['date'] + regions)
+    i = 0
+    for date in dates:
+        w.writerow([date] + [prevalences[region][i] for region in regions])
+        i += 1
+
+
+if len(sys.argv) != 2:
+    sys.exit("Usage: ./incidence.py input.csv > output.csv")
+
+with open(sys.argv[1]) as infile:
+    write_prevalence(infile, sys.stdout)
