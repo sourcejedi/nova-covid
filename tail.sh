a50cca6daad82b933466a73c6d3345a1ea9ebc05
Alan Jenkins <alan.christopher.jenkins@gmail.com>
2022-04-08 15:06:48 +0100

    tail.sh: fix after renaming files used

--- tail.sh
+++ tail.sh
@@ -12,9 +12,9 @@ for i in download/incidence_history/*.csv; do
 done
 
 file="$dir"/prevalence_history.csv
-headfile="$(ls out/prevalence_history.England_weighted/*.csv | head -n1)"
+headfile="$(ls out/prevalence_history.England/*.csv | head -n1)"
 head -n1 "$headfile" > "$file"
-for i in out/prevalence_history.England_weighted/*.csv; do
+for i in out/prevalence_history.England/*.csv; do
     tail -q -n1 "$i" >> "$file"
 done
 
