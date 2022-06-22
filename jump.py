d1434dd97cdb8175ddb0feaffbf47608fd0f46a5
Alan Jenkins <alan.christopher.jenkins@gmail.com>
2022-06-22 22:26:08 +0100

    jump.py: add 18-day average, to help compensate for recent patterns

--- jump.py
+++ jump.py
@@ -6,7 +6,7 @@ import csv
 from collections import OrderedDict
 import math
 
-SKIP_LAST_DAYS=2
+SKIP_LAST_DAYS=3
 COMPARE_LAST_DAYS=30
 
 def jump(indir, prefix, outfile):
@@ -90,15 +90,32 @@ def jump(indir, prefix, outfile):
                         except StopIteration:
                             return # ran out of days in current series
                 
-                all_dev = 0
-                for d in dates_to_compare():
-                    dev = (values[d] - prev_values[d]) / prev_values[d]
-                    dev = dev * dev
-                    all_dev += dev
-                all_dev = math.sqrt(all_dev / (COMPARE_LAST_DAYS-1))
+                def changes():
+                    for d in dates_to_compare():
+                        yield (values[d] - prev_values[d]) / prev_values[d]
+                changes = list(changes())
+
+                # Original metric
+                sum_squares = 0
+                for c in changes:
+                    sum_squares += c * c
+                change = math.sqrt(sum_squares / (len(changes) - 1))
+
+                # Compensate for 18-day cyclic pattern,
+                # visible in many but not all recent comparisons.
+                # The pattern sums to zero,
+                # so the simplest approach is to average it away.
+                sum_squares = 0
+                rolling_sum = sum(changes[:17])
+                for i in range(17, len(changes)):
+                    rolling_sum += changes[i]
+                    mean = rolling_sum / 18
+                    sum_squares += mean * mean
+                    rolling_sum -= changes[i-17]
+                change2 = math.sqrt(sum_squares / (len(changes) - 17 - 1))
 
                 date = next(reversed(values.keys()))
-                writer.writerow([date, all_dev])
+                writer.writerow([date, change, change2])
             prev_values = values
 
 outdir = Path('out/jump/')
