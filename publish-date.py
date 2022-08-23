30ae5396c34757381543eec3e0c40c1754f52368
Alan Jenkins <alan.christopher.jenkins@gmail.com>
2022-08-23 10:39:11 +0100

    publish-date.incidence.UK.v5.ods
    UK version, for alignment with the Reddit ZOE-bot.

--- publish-date.py
+++ publish-date.py
@@ -2,7 +2,7 @@
 from pathlib import Path
 import mmap
 
-def publish_date(indir, prefix, outfile):
+def publish_date(indir, prefix, out_en, out_uk):
     paths = list(indir.glob(prefix + '*.csv'))
     paths.sort()
 
@@ -13,23 +13,36 @@ def publish_date(indir, prefix, outfile):
         # Header line
         if path.name == 'incidence_20211003.csv':
             with path.open('rb') as infile:
-                outfile.write(next(infile))
+                header = next(infile)
+                out_en.write(header)
+                out_uk.write(header)
 
         with path.open('rb') as infile:
             # You *could* do this efficiently
             # without mmap, but why bother?
-            data = mmap.mmap(infile.fileno(), 0, prot=mmap.PROT_READ)
-            
-            j = len(data)
-            i = data.rfind(b'\n', 0, j)
-            last_en = data[i+1:j+1]
-            while not b',England,' in last_en:
-                j = i
+            def reversed_lines():
+                data = mmap.mmap(infile.fileno(), 0, prot=mmap.PROT_READ)
+                j = len(data)
                 i = data.rfind(b'\n', 0, j)
-                last_en = data[i+1:j+1]
+                yield data[i+1:j]
+                while i >= 0:
+                    j = i
+                    i = data.rfind(b'\n', 0, j)
+                    yield data[i+1:j+1]
 
-            outfile.write(last_en)
+            lines = reversed_lines()
+            line = next(lines)
+            while b',England,' not in line:
+                line = next(lines)
+            out_en.write(line)
+            
+            lines = reversed_lines()
+            line = next(lines)
+            while b',UK,' not in line:
+                line = next(lines)
+            out_uk.write(line)
 
 indir = Path('download/incidence/')
-with open('out/publish-date.incidence.England.v5.csv', 'wb') as outfile:
-    publish_date(indir, 'incidence_', outfile)
+with (open('out/publish-date.incidence.England.v5.csv', 'wb') as out_en,
+      open('out/publish-date.incidence.UK.v5.csv', 'wb') as out_uk):
+    publish_date(indir, 'incidence_', out_en, out_uk)
