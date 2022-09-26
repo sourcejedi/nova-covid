186a3395512a96d0f131c6dde0b5891b08a13948
Alan Jenkins <alan.christopher.jenkins@gmail.com>
2022-09-26 09:55:08 +0100

    fetch.sh: quick & dirty optimization
    if we already have a file from the end of 2021, then only look at
    downloading files from 2022-2029.

--- fetch.sh
+++ fetch.sh
@@ -3,6 +3,11 @@
 # Debug mode
 set -o xtrace
 
+dates="202[2-9]*"
+if [ ! -e download/incidence/incidence_20211231.csv ]; then
+    dates="202*"
+fi
+
 # Download ZOE files using gsutil from google-cloud-sdk.
 # See https://sourcejedi.github.io/2022/01/31/zoe-covid-study.html
 get() (
@@ -14,14 +19,14 @@ get() (
     gsutil -m cp -n "$get" .
 )
 
-get 'gs://covid-public-data/csv/incidence_202*.csv' \
+get "gs://covid-public-data/csv/incidence_${dates}.csv" \
         'incidence/' &&
-get 'gs://covid-public-data/csv/RevisedStats/prevalence_history_202*.csv' \
+get "gs://covid-public-data/csv/RevisedStats/prevalence_history_${dates}.csv" \
         'prevalence_history/' &&
-get 'gs://covid-public-data/csv/RevisedStats/incidence_history_202*.csv' \
+get "gs://covid-public-data/csv/RevisedStats/incidence_history_${dates}.csv" \
         'incidence_history/' &&
 
-get 'gs://terraform-covid-website-data-prod/personalized_assets/backup_files/newly_sick_table_202*.csv' \
+get "gs://terraform-covid-website-data-prod/personalized_assets/backup_files/newly_sick_table_${dates}.csv" \
         'newly_sick_table/' &&
 
 exit 0
